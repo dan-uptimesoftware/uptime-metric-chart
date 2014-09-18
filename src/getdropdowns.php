f549752262feb75553035ed21abfa9e3356a0994
dan branton <daniel.branton@uptimesoftware.com>
2014-09-17 20:27:10 -0400

    beginnings of switching the drop downs over to api calls instead of db queries

--- src/getdropdowns.php
+++ src/getdropdowns.php
@@ -14,10 +14,25 @@
 //lost profits even if uptime software has been advised of the possibility of such
 //damages.
 
+
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/classLoader.inc";
+
+
+session_start();
+
+$user_name =  $_SESSION['current_user']->getName();
+$user_pass = $_SESSION['current_user']->getPassword();
+
+session_write_close();
+
+
 // Set the JSON header
 header("Content-type: text/json");
 
+
 include("uptimeDB.php");
+include("uptimeApi.php");
 
 if (isset($_GET['query_type'])){
     $query_type = $_GET['query_type'];
@@ -60,59 +75,68 @@ $oneElement = array();
 $performanceData = array();
 //date_default_timezone_set('UTC');
 
-$db = new uptimeDB;
-if ($db->connectDB())
-{
-    echo "";
 
-}
-else
-{
- echo "unable to connect to DB exiting";    
- exit(1);
-}
 
+
+
+$uptime_api_username = $user_name;
+$uptime_api_password = $user_pass;
+$uptime_api_hostname = "localhost";     // up.time Controller hostname (usually localhost, but not always)
+$uptime_api_port = 9997;
+$uptime_api_version = "v1";
+$uptime_api_ssl = true;
+
+// Create API object
+$uptime_api = new uptimeApi($uptime_api_username, $uptime_api_password, $uptime_api_hostname, $uptime_api_port, $uptime_api_version, $uptime_api_ssl);
 
 
 
 // Enumerate elements with performance counters   
 if ($query_type == "elements_for_performance") {
-    $sql = "select e.entity_id, e.display_name
-            from entity e
-            join erdc_base eb on eb.erdc_base_id = e.defining_erdc_base_id
-            where e.entity_type_id not in (2, 3, 4, 5)
-            and e.entity_subtype_id in (1,21, 12)
-            and eb.name != 'MonitorDummyVmware'
-            and e.monitored = 1
-            order by e.display_name asc;
-            ";
-            
-        $result = $db->execQuery($sql);
-        
-        foreach ($result as $row) {
-            $json[$row['DISPLAY_NAME']] = $row['ENTITY_ID'];
-        }
-    
-    // Echo results as JSON
+    $elements = $uptime_api->getElements("type=Server&isMonitored=1");
+
+    foreach ($elements as $d) {
+        $json[$d['name']] = $d['id'];
+    }
+
+    //sort alphabeticaly on name instead of their order on ID
+    ksort($json);  
+
     echo json_encode($json);
 }
 
 // Enumerate elements with performance counters   
 elseif ($query_type == "groups_for_performance") {
-    $sql = "select entity_group_id, name from entity_group order by name asc;";
-            
-        $result = $db->execQuery($sql);
-        
-        foreach ($result as $row) {
-            $json[$row['NAME']] = $row['ENTITY_GROUP_ID'];
-        }
-    
-    // Echo results as JSON
+
+
+    $groups = $uptime_api->getGroups();
+
+    foreach ($groups as $d) {
+        $json[$d['name']] = $d['id'];
+    }
+
+    //sort alphabeticaly on name instead of their order on ID
+    ksort($json);  
+
     echo json_encode($json);
 }
 
 // Enumerate elements with performance counters   
 elseif ($query_type == "views_for_performance") {
+    
+    $db = new uptimeDB;
+    if ($db->connectDB())
+    {
+        echo "";
+
+    }
+    else
+    {
+        echo "unable to connect to DB exiting";    
+        exit(1);
+    }
+
+
     $sql = "select id, name from entity_view order by name asc;
             ";
             
@@ -122,6 +146,9 @@ elseif ($query_type == "views_for_performance") {
             $json[$row['NAME']] = $row['ID'];
         }
     
+    //sort alphabeticaly on name instead of their order on ID
+    ksort($json);    
+
     // Echo results as JSON
     echo json_encode($json);
 }
